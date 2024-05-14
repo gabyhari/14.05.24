bf1de2236a897e484355baba14b97c16258c1fef
gabyhari <142436298+gabyhari@users.noreply.github.com>
2024-05-14 12:32:16 -0300

    Add files via upload

---
+++ tabuleiro.js
@@ -0,0 +1,89 @@
+class Tabuleiro {
+    constructor (cor, x, y, linha, coluna, altura, largura, nome) {
+        this.cor = cor;
+        this.x = x;
+        this.y = y;
+        this.linha = linha;
+        this.coluna = coluna;
+        this.altura = altura;
+        this.largura = largura;
+        this.nome = nome;
+    }
+}
+
+const cor1 = "branco"
+const cor2 = "preto"
+const medida = "px"
+const larguraPadrao = 30;
+const alturaPadrao = 30;
+
+celula[0] = new Tabuleiro(cor1,"0", "0", "8", "a", alturaPadrao, larguraPadrao,"celula_0");
+celula[1] = new Tabuleiro(cor2,"30", "0", "8", "b", alturaPadrao, larguraPadrao,"celula_1" );
+celula[2] = new Tabuleiro(cor2,"90", "0", "8", "d", alturaPadrao, larguraPadrao, "celula_2");
+celula[3] = new Tabuleiro(cor1,"120", "0", "8", "e", alturaPadrao, larguraPadrao,"celula_3" );
+celula[4] = new Tabuleiro(cor2,"150", "0", "8", "f", alturaPadrao, larguraPadrao, "celula_4");
+celula[5] = new Tabuleiro(cor1,"180", "0", "8", "g", alturaPadrao, larguraPadrao, "celula_5");
+celula[6] = new Tabuleiro(cor2,"210", "0", "8", "h", alturaPadrao, larguraPadrao, "celula_6");
+
+celula[7] = new Tabuleiro(cor2,"0", "30", "7", "a", alturaPadrao, larguraPadrao, "celula_0");
+celula[8] = new Tabuleiro(cor1,"30", "30", "7", "b", alturaPadrao, larguraPadrao, );
+celula[9] = new Tabuleiro(cor2,"60", "30", "7", "c", alturaPadrao, larguraPadrao,);
+celula[10] = new Tabuleiro(cor1,"90", "30", "7", "d", alturaPadrao, larguraPadrao,);
+celula[11] = new Tabuleiro(cor2,"120", "30", "7", "e", alturaPadrao, larguraPadrao,);
+celula[12] = new Tabuleiro(cor1,"150", "30", "7", "f", alturaPadrao, larguraPadrao,);
+celula[13] = new Tabuleiro(cor2,"180", "30", "7", "g", alturaPadrao, larguraPadrao,);
+celula[14] = new Tabuleiro(cor1,"210", "30", "7", "h", alturaPadrao, larguraPadrao,);
+
+celula[15] = new Tabuleiro(cor1,"0", "60", "6", "a", alturaPadrao, larguraPadrao,);
+celula[16] = new Tabuleiro(cor2,"30", "60", "6", "b", alturaPadrao, larguraPadrao,);
+celula[17] = new Tabuleiro(cor1,"60", "60", "6", "c", alturaPadrao, larguraPadrao,);
+celula[18] = new Tabuleiro(cor2,"90", "60", "6", "d", alturaPadrao, larguraPadrao,);
+celula[19] = new Tabuleiro(cor1,"120", "60", "6", "e", alturaPadrao, larguraPadrao,);
+celula[20] = new Tabuleiro(cor2,"150", "60", "6", "f", alturaPadrao, larguraPadrao,);
+celula[21] = new Tabuleiro(cor1,"180", "60", "6", "g", alturaPadrao, larguraPadrao,);
+celula[22] = new Tabuleiro(cor2,"210", "60", "6", "h", alturaPadrao, larguraPadrao,);
+
+celula[23] = new Tabuleiro(cor2,"0", "90", "5", "a", alturaPadrao, larguraPadrao,);
+celula[24] = new Tabuleiro(cor1,"30", "90", "5", "b", alturaPadrao, larguraPadrao,);
+celula[25] = new Tabuleiro(cor2,"60", "90", "5", "c", alturaPadrao, larguraPadrao,);
+celula[26] = new Tabuleiro(cor1,"90", "90", "5", "d", alturaPadrao, larguraPadrao,);
+celula[27] = new Tabuleiro(cor2,"120", "90", "5", "e", alturaPadrao, larguraPadrao,);
+celula[28] = new Tabuleiro(cor1,"150", "90", "5", "f", alturaPadrao, larguraPadrao,);
+celula[29] = new Tabuleiro(cor2,"180", "90", "5", "g", alturaPadrao, larguraPadrao,);
+celula[30] = new Tabuleiro(cor1,"210", "90", "5", "h", alturaPadrao, larguraPadrao,);
+
+celula[31] = new Tabuleiro(cor1,"0", "120", "4", "a", alturaPadrao, larguraPadrao,);
+celula[32] = new Tabuleiro(cor2,"30", "120", "4", "b", alturaPadrao, larguraPadrao);
+celula[33] = new Tabuleiro(cor1,"60", "120", "4", "c", alturaPadrao, larguraPadrao,);
+celula[34] = new Tabuleiro(cor2,"90", "120", "4", "d", alturaPadrao, larguraPadrao,);
+celula[35] = new Tabuleiro(cor1,"120", "120", "4", "e", alturaPadrao, larguraPadrao,);
+celula[36] = new Tabuleiro(cor2,"150", "120", "4", "f", alturaPadrao, larguraPadrao,);
+celula[37] = new Tabuleiro(cor1,"180", "120", "4", "g", alturaPadrao, larguraPadrao,);
+celula[38] = new Tabuleiro(cor2,"210", "120", "4", "h", alturaPadrao, larguraPadrao,);
+
+celula[39] = new Tabuleiro(cor2,"0", "150", "3", "a", alturaPadrao, larguraPadrao,);
+celula[40] = new Tabuleiro(cor1,"30", "150", "3", "b", alturaPadrao, larguraPadrao,);
+celula[41] = new Tabuleiro(cor2,"60", "150", "3", "c", alturaPadrao, larguraPadrao,);
+celula[42] = new Tabuleiro(cor1,"90", "150", "3", "d", alturaPadrao, larguraPadrao,);
+celula[43] = new Tabuleiro(cor2,"120", "150", "3", "e", alturaPadrao, larguraPadrao,);
+celula[44] = new Tabuleiro(cor1,"150", "150", "3", "f", alturaPadrao, larguraPadrao,);
+celula[45] = new Tabuleiro(cor2,"180", "150", "3", "g", alturaPadrao, larguraPadrao,);
+celula[46] = new Tabuleiro(cor1,"210", "150", "3", "h", alturaPadrao, larguraPadrao,);
+
+celula[48] = new Tabuleiro(cor1,"0", "180", "2", "a", alturaPadrao, larguraPadrao,);
+celula[49] = new Tabuleiro(cor2,"30", "180", "2", "b", alturaPadrao, larguraPadrao,);
+celula[50] = new Tabuleiro(cor1,"60", "180", "2", "c", alturaPadrao, larguraPadrao,);
+celula[51] = new Tabuleiro(cor2,"90", "180", "2", "d", alturaPadrao, larguraPadrao,);
+celula[52] = new Tabuleiro(cor1,"120", "180", "2", "e", alturaPadrao, larguraPadrao,);
+celula[53] = new Tabuleiro(cor2,"150", "180", "2", "f", alturaPadrao, larguraPadrao,);
+celula[54] = new Tabuleiro(cor1,"180", "180", "2", "g", alturaPadrao, larguraPadrao,);
+celula[55] = new Tabuleiro(cor2,"210", "180", "2", "h", alturaPadrao, larguraPadrao,);
+
+celula[56] = new Tabuleiro(cor2,"0", "210", "1", "a", alturaPadrao, larguraPadrao,);
+celula[57] = new Tabuleiro(cor1,"30", "210", "1", "b", alturaPadrao, larguraPadrao,);
+celula[58] = new Tabuleiro(cor2,"60", "210", "1", "c", alturaPadrao, larguraPadrao,);
+celula[59] = new Tabuleiro(cor1,"90", "210", "1", "d", alturaPadrao, larguraPadrao,);
+celula[60] = new Tabuleiro(cor2,"120", "210", "1", "e", alturaPadrao, larguraPadrao,);
+celula[61] = new Tabuleiro(cor1,"150", "210", "1", "f", alturaPadrao, larguraPadrao,);
+celula[62] = new Tabuleiro(cor2,"180", "210", "1", "g", alturaPadrao, larguraPadrao,);
+celula[63] = new Tabuleiro(cor1,"210", "210", "1", "h", alturaPadrao, larguraPadrao,); 
